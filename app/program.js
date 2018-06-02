ba5b096d65410fb695d163e77b0a6bf291ef2236
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-06-02 16:27:14 +0200

    divid into files and add time converter

--- app/program.js
+++ app/program.js
@@ -1,11 +1,14 @@
+var os = require('os');
+var OSinfo = require('../modules/OSinfo');
+
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
     // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
+	console.log('wpisz: /exit, /info, /sayhello, getOSinfo, ...');
     if (input !== null) {
         var instruction = input.toString().trim(); //trim pozbywa się białych znaków
-		
 		switch (instruction) {
 			case '/exit': 
 				process.stdout.write('Quitting app!\n');
@@ -14,8 +17,16 @@ process.stdin.on('readable', function() {
 			case '/info':
 				console.log('Język: ' + process.env.LANG + ', wersja: '+ process.versions.node);
 				break;
+			case '/sayhello':
+                process.stdout.write('hello!\n');
+                break;
+			case '/getOSinfo':
+				process.stdout.write('Info o systemie: \n');
+				OSinfo.print();
+				break;
 			default: 
 				process.stderr.write('Wrong instruction! \n');
 		}
     }
 });
+
